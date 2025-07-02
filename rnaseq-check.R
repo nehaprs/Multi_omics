1e8487aa92c67da1c4cf22b56b380de35f74f1f9
nehaprs <nehaprs@gmail.com>
2025-07-02 10:20:58 -0400

    Add glmQLFit analysis for mRNA data
    Introduces an alternative analysis pipeline using edgeR's glmQLFit and glmQLFTest for mRNA data. The results are saved to a new file 'mrna_glmqlfit.xlsx' for comparison with the previous method.

--- rnaseq-check.R
+++ rnaseq-check.R
@@ -25,3 +25,26 @@ results$PValue <- lrt$table$PValue
 results$FDR <- p.adjust(results$PValue, method = "BH") 
 mrna = cbind(mrna, results)
 writexl::write_xlsx(mrna,"mrna_rechecked.xlsx")
+
+#######################
+#try glmQLFit
+########################
+
+
+mrna <- read_excel("~/BINF/BINF_old/multi-omics/ADAM9 KD HCT116 proteomics/Protein and mRNA/DEGs and DEPs/DEGs/mRNA All Results.xlsx")
+counts = mrna[c(2:13)]
+
+group = factor(c(rep("Control", 6), rep("Treatment", 6)))
+
+dge = DGEList(counts = counts, group = group)
+dge = calcNormFactors(dge)
+dge = estimateDisp(dge)
+#fit = glmFit(dge)
+#lrt = glmLRT(fit, coef=2)
+fit = glmQLFit(dge)
+lrt = glmQLFTest(fit, coef=2)
+results = lrt$table
+results$PValue <- lrt$table$PValue     
+results$FDR <- p.adjust(results$PValue, method = "BH") 
+mrna = cbind(mrna, results)
+writexl::write_xlsx(mrna,"mrna_glmqlfit.xlsx")
